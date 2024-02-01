2e3e4feee8068accf5a3d0aeaba745f9161a605a
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-02 01:59:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function volume_sphere() {
-    //Write your code here
-	let r=document.getElementById("radius").value;
-	let v=document.getElementById("volume").value;
-	let pi=Math.PI;
-	let volume=Math.floor(4/3)*Math.pi*r**3;
-     v=volume;
-} 
-
+function volume_sphere()
+ {
+  var volume;
+  var radius = document.getElementById('radius').value;
+  radius = Math.abs(radius);
+  volume = (4/3) * Math.PI * Math.pow(radius, 3);
+  volume = volume.toFixed(4);
+  document.getElementById('volume').value = volume;
+  return false;
+ } 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
